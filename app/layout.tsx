ab5d6777196d17aa50f96e2ebd600de955702774
Rahul Chandra <rahul@projecteclipse.org>
2024-09-02 19:29:53 -0400

    umm hopefully github pages works now?

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,18 @@ import "./global.css";
 import Navbar from "./navbar";
 import Title from "./page";
 import About from "./about";
-export const Layout = ({ children }: { children: React.ReactNode }) => {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html className="scroll-smooth">
-      <head>
-        <body>
-          <Navbar />
-          {children}
-        </body>
-      </head>
+      <head></head>
+      <body>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
-};
-export default Layout;
+}
